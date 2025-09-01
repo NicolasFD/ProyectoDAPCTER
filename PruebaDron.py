be84666881eebac092256134bfa4519c773bc346
NicolasFD <nocolascity18@gmail.com>
2025-09-01 09:23:59 -0500

    Segundo commit

--- PruebaDron.py
+++ PruebaDron.py
@@ -3,3 +3,15 @@ import time
 
 # Connect to the vehicle
 vehicle = connect('udp:127.0.0.1:14550', wait_ready=True)
+# Arm and takeoff
+vehicle.mode = VehicleMode("GUIDED")
+vehicle.armed = True
+while not vehicle.armed:
+    time.sleep(1)
+vehicle.simple_takeoff(10)  # Take off to 10 meters
+# Wait until the drone reaches the altitude
+while True:
+    if vehicle.location.global_relative_frame.alt >= 9.5:
+        break
+    time.sleep(1)
+vehicle.mode = VehicleMode("LAND")
