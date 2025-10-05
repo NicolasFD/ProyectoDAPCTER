93d0f550d95dea60adf2a4d2e0491663e36458f7
NicolasFD <nocolascity18@gmail.com>
2025-10-04 19:09:08 -0500

    cuarto commit

---
+++ VisionArtificial2.py
@@ -0,0 +1,78 @@
+import cv2
+from ultralytics import YOLO
+import numpy as np
+import os
+import time
+
+# === Cargar imagen de prueba ===
+imagen_path = "031R.jpg"   # pon aquí el nombre de tu archivo
+frame = cv2.imread("031R.jpg")
+
+if frame is None:
+    raise FileNotFoundError("No se pudo leer la imagen, revisa la ruta.")
+
+# === Cargar el modelo YOLO ===
+model = YOLO("ProyectoDAPCTER/best.pt")
+
+classNames = ["Panel-Hotspots"]
+
+output_dir = "detecciones_guardadas"
+os.makedirs(output_dir, exist_ok=True)
+
+# === Procesar la imagen ===
+results = model(frame, stream=True)
+
+for res in results:
+    boxes = res.boxes
+    if boxes is None or len(boxes) == 0:
+        continue
+
+    for box in boxes:
+        x1, y1, x2, y2 = box.xyxy[0]
+        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+
+        conf = float(box.conf[0])
+        cls = int(box.cls[0])
+        label = classNames[cls] if cls < len(classNames) else f"Class {cls}"
+
+        # Paso 1: Matriz de valores
+        roi = frame[y1:y2,x1:x2]
+        gray_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+
+        # Paso 2: Media y máscara
+        mean_val = np.mean(gray_roi)
+        mask = gray_roi > mean_val+100
+
+        # Paso 3: Guardar si hay píxeles mayores a la media
+        if np.any(mask):
+            filename = f"{output_dir}/{label}_{int(time.time()*1000)}.jpg"
+            cv2.imwrite(filename, roi)
+
+    
+    for box in boxes:
+        x1, y1, x2, y2 = box.xyxy[0]
+        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+
+        conf = float(box.conf[0])
+        cls = int(box.cls[0])
+        label = classNames[cls] if cls < len(classNames) else f"Class {cls}"
+
+        # Paso 1: Matriz de valores
+        roi = frame[y1:y2,x1:x2]
+        gray_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+        print(gray_roi)
+
+        # Paso 2: Media y máscara
+        mean_val = np.mean(gray_roi)
+        mask = gray_roi > mean_val
+
+        #Dibujar bounding box
+        text = f"{label} {conf:.2f} Mean:{mean_val:.1f}"
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
+        cv2.putText(frame, text, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5, (255, 0, 0), 2)
+
+# Mostrar la imagen con detecciones
+cv2.imshow("Resultado", frame)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
